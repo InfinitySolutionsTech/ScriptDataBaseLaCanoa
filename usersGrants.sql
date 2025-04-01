c3d115bf3cde4417728591ee3d056987da11fe13
hollmanPinto <92654372+hollmanPinto@users.noreply.github.com>
2025-04-01 18:53:37 -0500

    add grants over burgers

--- usersGrants.sql
+++ usersGrants.sql
@@ -2,3 +2,7 @@ CREATE USER 'lacanoa_user'@'%' IDENTIFIED BY '5T0r3Pr8S01ut20ns';
 GRANT SELECT, INSERT, UPDATE, DELETE ON lacanoa.* TO 'lacanoa_user'@'%';
 FLUSH PRIVILEGES;
 SHOW GRANTS FOR 'lacanoa_user'@'%';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON lasburguers.* TO 'lacanoa_user'@'%';
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'lacanoa_user'@'%';
