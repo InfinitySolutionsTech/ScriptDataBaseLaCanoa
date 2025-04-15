5459cb9767fdd2c23c4299de519a048b6cf0e23c
hollmanPinto <92654372+hollmanPinto@users.noreply.github.com>
2025-04-15 17:08:06 -0500

    FIX reports

---
+++ modifications_data.sql
@@ -0,0 +1,7 @@
+update reports
+set report_name = 'Reporte General de Facturacion'
+where id = 1 or id = 3;
+
+update reports
+set report_name = 'Reporte productos ofrecidos'
+where id = 2 or id = 4;
